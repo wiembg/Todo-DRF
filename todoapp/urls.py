720fc1d8ee58889933f61ffa19d7bc482e314e71
wiembg <48589743+wiembg@users.noreply.github.com>
2022-10-31 16:07:23 +0100

    Registration

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import *
+from .views import SignUpView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = format_suffix_patterns([
-    path('',api_root),
-    
+    path('',api_root),   
+    path("signup/", SignUpView.as_view(), name="signup"),
     path('todos/',ListTodo.as_view(),name='task-list'),
     path('create/',CreateTodo.as_view(),name='create'),
     path('todos/<int:pk>/',DetailTodo.as_view(),name='task-detail'),
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import generics
@@ -7,6 +8,8 @@ from .permissions import IsOwnerOrReadOnly
 from rest_framework.decorators import api_view
 from rest_framework.reverse import reverse
 from .serializers import *
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 # Create your views here.
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -14,7 +17,11 @@ def api_root(request, format=None):
         'users': reverse('user-list', request=request, format=format),
         'tasks': reverse('task-list', request=request, format=format)
     })
-    
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("user-list")
+    template_name = "signup.html"
 class UserList(generics.ListAPIView):#userlist
     queryset = User.objects.all()
     serializer_class = UserSerializer

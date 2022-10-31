b1e91910865bed921e6a144401f6dc44bb768f0f
wiembg <48589743+wiembg@users.noreply.github.com>
2022-10-31 17:42:18 +0100

    Signals

--- todoapp/admin.py
+++ todoapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Task
+from .models import *
 
 admin.site.register(Task)
+admin.site.register(UserProfile)
 #admin@admi.com
 #admin
 #admin123
--- todoapp/models.py
+++ todoapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 # Create your models here.
 
 class Task(models.Model):
@@ -15,3 +16,15 @@ class Task(models.Model):
 
     class Meta:
         ordering = ['created']
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    def __str__(self):
+        return self.user.username
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)        

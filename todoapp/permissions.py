5b5c15ed040993e820fbb4b669e63c53bf10b138
wiembg <48589743+wiembg@users.noreply.github.com>
2022-11-06 17:12:45 +0100

    permissions

--- todoapp/permissions.py
+++ todoapp/permissions.py
@@ -1,9 +1,10 @@
 from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
+    message = 'Editing tasks is restricted to the author only.'
     def has_object_permissions(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        print(request.user)
+       
         return obj.owner == request.user
                 #if obj.owner is equal to request.user it will return True, otherwise False:return the results belonging to the owner
